08adf538d9c6d8793d8d84bdd2b304ac4c17b6c5
Riean Esteves <89774815+Rieesteves@users.noreply.github.com>
2025-11-12 23:04:31 +0530

    Register service worker on window load

--- script.js
+++ script.js
@@ -604,3 +604,12 @@ if (clearScoresBtn) {
   init();
 
 }); // DOMContentLoaded end
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(() => console.log('✅ Service Worker Registered!'))
+      .catch(err => console.log('❌ SW registration failed:', err));
+  });
+}
+
